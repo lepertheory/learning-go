0930de553e4888de7e2eb6efbe8d1abbd2c2acb4
David Corby <david.corby@gmail.com>
2023-12-03 00:27:43 -0700

    './lepertheory.net -h -h --help' prints '3'. Too easy. Something, many things, must be wrong.

--- getopt-long/getopt-long.go
+++ getopt-long/getopt-long.go
@@ -1,14 +1,41 @@
 package getopt_long
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"unsafe"
 )
 
 // #include <getopt.h>
 // #include <stdlib.h>
 import "C"
+
+type ArgRequirement int
+const (
+	ArgNotAllowed ArgRequirement = iota
+	ArgOptional   ArgRequirement = iota
+	ArgRequired   ArgRequirement = iota
+)
+
+type Option struct {
+	Name *string
+	Short *string
+	Required bool
+	Arg ArgRequirement
+}
+
+type OptionResult struct {
+	SetCount  int
+	Arguments []string
+}
+
+type GetOpt struct {
+	Options   []Option
+	Arguments []string
+	Results   map[Option]OptionResult
+}
 
 func getCString(value string) (*C.char, func()) {
 	retString := C.CString(value)
@@ -30,17 +57,119 @@ func getCSlice[TFrom any, TTo any](values []TFrom, converter func(TFrom)(TTo, fu
 	}
 }
 
+func getOptstring(options []Option) string {
+	var retval strings.Builder
+	for _, option := range options {
+		if option.Short != nil {
+			retval.WriteString(*option.Short)
+			retval.WriteString(option.Arg.toOptstring())
+		}
+	}
+	return retval.String()
+}
+
+func (o ArgRequirement) toOptstring() string {
+	switch o {
+	case ArgNotAllowed: return ""
+	case ArgOptional:   return "::"
+	case ArgRequired:   return ":"
+	default:
+		// TODO: Is this the right way to raise a programming error?
+		panic(errors.New(fmt.Sprintf("Unexpected ArgRequirement value: %d", o)))
+	}
+}
+
+func (o ArgRequirement) toHasArg() C.int {
+	switch o {
+	case ArgNotAllowed: return C.no_argument
+	case ArgOptional:   return C.optional_argument
+	case ArgRequired:   return C.required_argument
+	default:
+		// TODO: Is this the right way to raise a programming error?
+		panic(errors.New(fmt.Sprintf("Unexpected ArgRequirement value: %d", o)))
+	}
+}
+
+func (o *GetOpt) Process() {
+
+	argvSlice, argvSliceCloser := getCSlice(
+		o.Arguments,
+		func(from string)(*C.char, func()) { return getCString(from) },
+	)
+	defer argvSliceCloser()
+	argv := &(argvSlice[0])
+	argc := C.int(len(argvSlice))
+
+	optstringBacking := getOptstring(o.Options)
+	optstring, optstringCloser := getCString(optstringBacking)
+	defer optstringCloser()
+
+	optionsSlice, optionsSliceCloser := getCSlice(
+		o.Options,
+		func(from Option)(C.struct_option, func()) {
+			name, nameCloser := getCString(*from.Name)
+
+			// FIXME: What happens if .toHasArg() panics and is recovered? I'm missing something,
+			//        this shouldn't be so painful.
+			return C.struct_option{
+				name: name,
+				has_arg: from.Arg.toHasArg(),
+				flag: (*C.int)(C.NULL),
+				val: C.int(0),
+			}, nameCloser
+		},
+	)
+	defer optionsSliceCloser()
+
+	longOpts := make(map[string]*Option)
+	shortOpts := make(map[string]*Option)
+	for _, opt := range(o.Options) {
+		if opt.Name	!= nil {
+			longOpts[*opt.Name] = &opt
+		}
+		if opt.Short != nil {
+			shortOpts[*opt.Short] = &opt
+		}
+	}
+
+	// FIXME: Is this the right place to initialize this map?
+	o.Results = map[Option]OptionResult{}
+	optind := C.int(0)
+	for {
+		// FIXME: Handle option arguments.
+		result := C.getopt_long(argc, argv, optstring, &(optionsSlice[0]), &optind)
+		if result == -1 {
+			break
+		}
+
+		var opt *Option
+		if result == 0 {
+			opt = longOpts[C.GoString(optionsSlice[optind].name)]
+		} else {
+			opt = shortOpts[string(rune(result))]
+		}
+		// TODO: There has to be a reason this is such a pain, and it has to be that I'm doing
+		//       something wrong.
+		// FIXME: Initialize at the right time.
+		optResult := o.Results[*opt]
+		optResult.SetCount++
+		o.Results[*opt] = optResult
+	}
+
+}
+
 func Fart() {
 	fmt.Println("fart")
 
 	argvArr, freeArgvArr := getCSlice(os.Args, func(from string)(*C.char, func()) { return getCString(from); })
 	defer freeArgvArr()
-
 	argv := &(argvArr[0])
 	argc := C.int(len(argvArr))
+
 	//goland:noinspection SpellCheckingInspection
 	optstring, freeOptstring := getCString("h")
 	defer freeOptstring()
+
 	opts := [1]C.struct_option{{
 		name: C.CString("help"),
 		has_arg: C.no_argument,
@@ -48,10 +177,7 @@ func Fart() {
 		val: C.int(0),
 	}}
 	for i := range opts {
-		defer func(idx int) {
-			C.free(unsafe.Pointer(opts[idx].name))
-			fmt.Printf("freed %d\n", idx)
-		}(i)
+		defer C.free(unsafe.Pointer(opts[i].name))
 	}
 	for i, s := range os.Args {
 		argvArr[i] = C.CString(s)
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"os"
+
 	gol "lepertheory.net/getopt-long"
 )
 
+func pointer[T any](val T) *T {
+	return &val
+}
+
 func main() {
-	gol.Fart()
+	getopt := gol.GetOpt{
+		Options: []gol.Option{
+			{
+				Name: pointer("help"),
+				Short: pointer("h"),
+				Required: false,
+				Arg: gol.ArgNotAllowed,
+			},
+		},
+		Arguments: os.Args,
+	}
+	getopt.Process()
+	println(getopt.Results[getopt.Options[0]].SetCount)
 }
